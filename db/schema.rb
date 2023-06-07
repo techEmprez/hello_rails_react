6a261f496094d12ec5d7e44ade3256e35c8af22e
Choice Osobor <choiceosobor@gmail.com>
2023-06-07 02:47:03 +0100

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_06_07_013638) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "greetings", force: :cascade do |t|
     t.string "greeting"
     t.datetime "created_at", null: false
