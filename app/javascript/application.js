a3e65ab0fbb91bf687ad1998908eef10c3268e22
Choice Osobor <choiceosobor@gmail.com>
2023-06-07 01:36:48 +0100

    add react-router

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-function App() {
-  return (<h1>Hello World!</h1>);
-}
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import store from "./redux/store";
+import { Provider } from "react-redux";
 
 ReactDOM.render(
-  <App/>,
-  document.getElementById('root'),
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
